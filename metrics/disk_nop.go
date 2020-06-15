fc3a1f4690fa4a4a9718fab7274c280a7c7c971b
Santos-Kim <davinci.github2@gmail.com>
2020-06-15 17:54:20 +0900

    Update metrix and dacstats files. - metrics.go's func Init enables or disables the metrics system. Since we need this to run before any other code gets to create meters and timers, we'll actually do an ugly hack and peek into the command line args for the metrics flag. - Service structure implements an Davinci netstats reporting daemon that pushes local chain statistics up to a monitoring server.

---
+++ metrics/disk_nop.go
@@ -0,0 +1,26 @@
+// Copyright 2018 The go-Dacchain Authors
+// This file is part of the go-Dacchain library.
+//
+// The go-Dacchain library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-Dacchain library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-Dacchain library. If not, see <http://www.gnu.org/licenses/>.
+
+// +build !linux
+
+package metrics
+
+import "errors"
+
+// ReadDiskStats retrieves the disk IO stats belonging to the current process.
+func ReadDiskStats(stats *DiskStats) error {
+	return errors.New("Not implemented")
+}
